8a6151dad267f2f32f24a6b5c6ec0e0bd08c7130
CemM96 <cemmehm@hotmail.com>
2023-11-17 22:39:35 +0000

    removed unnecessary space

---
+++ Pong/pong_game.py
@@ -0,0 +1,83 @@
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.vector import Vector
+from random import randint
+
+
+class PongPaddle(Widget):
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            ball.velocity_x *= -1.2
+
+
+class PongBall(Widget):
+    velocity_x = NumericProperty(0)
+    velocity_y = NumericProperty(0)
+    velocity = ReferenceListProperty(velocity_x, velocity_y)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
+
+    def reset(self):
+        self.pos = Vector(0, 0)
+        self.velocity = Vector(0, 0)
+
+
+class PongGame(Widget):
+
+    ball = ObjectProperty(None)
+    player_1 = ObjectProperty(None)
+    player_2 = ObjectProperty(None)
+    player_1_score = NumericProperty(0)
+    player_2_score = NumericProperty(0)
+
+    def serve_ball(self):
+
+        self.ball.center = self.center
+
+        while True:
+            angle = randint(0, 359)
+            if (
+                    not (60 <= angle <= 120)
+                    and not (240 <= angle <= 300)
+                    and not (0 <= angle <= 30)
+                    and not (330 <= angle <= 359)
+                    and not (150 <= angle <= 210)
+            ):
+                break
+
+        self.ball.velocity = Vector(8, 0).rotate(angle)
+
+    def serve_button_pressed(self):
+        self.ids.serve_button.opacity = 0
+        self.ids.serve_button.disabled = True
+
+    def update(self, dt):
+
+        self.ball.move()
+
+        if (self.ball.y < 0) or (self.ball.y > self.height - 50):
+            self.ball.velocity_y *= -1
+
+        if self.ball.x < 0:
+            self.ball.center = self.center
+            self.ball.velocity = Vector(0, 0)
+            self.player_1_score += 1
+            self.ids.serve_button.opacity = 1
+            self.ids.serve_button.disabled = False
+        if self.ball.x > self.width - 50:
+            self.ball.center = self.center
+            self.ball.velocity = Vector(0, 0)
+            self.player_2_score += 1
+            self.ids.serve_button.opacity = 1
+            self.ids.serve_button.disabled = False
+
+        self.player_1.bounce_ball(self.ball)
+        self.player_2.bounce_ball(self.ball)
+
+    def on_touch_move(self, touch):
+        if touch.x < self.width / 4:
+            self.player_1.center_y = touch.y
+        if touch.x > self.width * 3/4:
+            self.player_2.center_y = touch.y
--- main.py
+++ main.py
@@ -1,11 +1,55 @@
-from Blackjack import game_loop
+from Pong.pong_game import PongGame
+from kivymd.app import MDApp
+from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.screen import MDScreen
+from kivy.lang import Builder
+from kivy.clock import Clock
+from kivy.core.window import Window
 
-player_balance = 1000
+
+class MenuScreen(MDScreen):
+    pass
 
 
-def main():
-    game_loop(player_balance)
+class SettingsScreen(MDScreen):
+    def back_to_menu(self):
+        self.manager.current = 'menu'
 
 
-if __name__ == "__main__":
-    main()
+class GameScreen(MDScreen):
+    def __init__(self, **kwargs):
+        super(GameScreen, self).__init__(**kwargs)
+        self.game = PongGame()
+        Clock.schedule_interval(self.game.update, 1.0 / 60.0)
+        self.add_widget(self.game)
+
+
+class PongApp(MDApp):
+    data = {
+        'Python': 'language-python',
+        'PHP': 'language-php',
+        'C++': 'language-cpp',
+    }
+
+    def build(self):
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "DeepPurple"
+        self.theme_cls.material_style = "M3"
+
+        Builder.load_file('../CemsGameApp/Pong/pong_game.kv')
+        Builder.load_file('menu.kv')
+        Builder.load_file('settings.kv')
+
+        sm = MDScreenManager()
+        sm.add_widget(MenuScreen(name='menu'))
+        sm.add_widget(SettingsScreen(name='settings'))
+        sm.add_widget(GameScreen(name='game'))
+        return sm
+
+    def on_start(self):
+        # Window.fullscreen = 'auto'
+        Window.size = (1280, 720)
+
+
+if __name__ == '__main__':
+    PongApp().run()
