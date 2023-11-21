e0c567e44a3ac80bbb5c42fcf3f94d225ffa17c7
CemM96 <cemmehm@hotmail.com>
2023-11-21 21:34:43 +0000

    -Added a score limit to pong game -added login screen -added background to menus -added temporary splash screen

--- Pong/pong_game.py
+++ Pong/pong_game.py
@@ -2,13 +2,14 @@ from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
 from random import randint
+from kivy.clock import Clock
 
 
 class PongPaddle(Widget):
 
     def bounce_ball(self, ball):
         if self.collide_widget(ball):
-            ball.velocity_x *= -1.2
+            ball.velocity_x *= -1.5
 
 
 class PongBall(Widget):
@@ -18,10 +19,6 @@ class PongBall(Widget):
 
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos
-
-    def reset(self):
-        self.pos = Vector(0, 0)
-        self.velocity = Vector(0, 0)
 
 
 class PongGame(Widget):
@@ -47,11 +44,29 @@ class PongGame(Widget):
             ):
                 break
 
-        self.ball.velocity = Vector(8, 0).rotate(angle)
+        self.ball.velocity = Vector(12, 0).rotate(angle)
 
     def serve_button_pressed(self):
+        self.ids.exit_button.opacity = 0
+        self.ids.exit_button.disabled = True
+
         self.ids.serve_button.opacity = 0
         self.ids.serve_button.disabled = True
+
+        self.ball.velocity = Vector(0, 0)
+        Clock.schedule_interval(self.update, 1.0 / 60.0)
+        Clock.schedule_once(lambda dt: self.serve_ball(), 0.8)
+
+    def end_game(self):
+        self.player_1_score = 0
+        self.player_2_score = 0
+        self.ball.center = self.center
+        self.ball.velocity = Vector(0, 0)
+        Clock.unschedule(self.update)
+        self.ids.exit_button.opacity = 1
+        self.ids.exit_button.disabled = False
+        self.ids.serve_button.opacity = 1
+        self.ids.serve_button.disabled = False
 
     def update(self, dt):
 
@@ -64,17 +79,24 @@ class PongGame(Widget):
             self.ball.center = self.center
             self.ball.velocity = Vector(0, 0)
             self.player_1_score += 1
-            self.ids.serve_button.opacity = 1
-            self.ids.serve_button.disabled = False
+            Clock.unschedule(self.update)
+            self.serve_button_pressed()
+
         if self.ball.x > self.width - 50:
             self.ball.center = self.center
             self.ball.velocity = Vector(0, 0)
             self.player_2_score += 1
-            self.ids.serve_button.opacity = 1
-            self.ids.serve_button.disabled = False
+            Clock.unschedule(self.update)
+            self.serve_button_pressed()
 
         self.player_1.bounce_ball(self.ball)
         self.player_2.bounce_ball(self.ball)
+
+        if self.player_1_score == 10:
+            self.end_game()
+
+        if self.player_2_score == 10:
+            self.end_game()
 
     def on_touch_move(self, touch):
         if touch.x < self.width / 4:
--- main.py
+++ main.py
@@ -5,15 +5,26 @@ from kivymd.uix.screen import MDScreen
 from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.core.window import Window
+from kivy.uix.screenmanager import FadeTransition
+
+
+class SplashScreen(MDScreen):
+    pass
+
+
+class LoginScreen(MDScreen):
+    pass
 
 
 class MenuScreen(MDScreen):
     def switch_to_settings(self):
+        self.manager.transition.direction = 'left'
         self.manager.current = 'settings'
 
 
 class SettingsScreen(MDScreen):
     def back_to_menu(self):
+        self.manager.transition.direction = 'right'
         self.manager.current = 'menu'
 
 
@@ -21,35 +32,53 @@ class PongGameScreen(MDScreen):
     def __init__(self, **kwargs):
         super(PongGameScreen, self).__init__(**kwargs)
         self.game = PongGame()
-        Clock.schedule_interval(self.game.update, 1.0 / 60.0)
         self.add_widget(self.game)
 
 
 class GameApp(MDApp):
-    def __init__(self):
-        super().__init__()
-        self.orientation = 'landscape'
-
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Cyan"
+        self.theme_cls.primary_palette = "DeepPurple"
         self.theme_cls.primary_hue = "500"
+        self.theme_cls.accent_palette = "DeepOrange"
+        self.theme_cls.accent_hue = "900"
         self.theme_cls.material_style = "M3"
 
         Builder.load_file('../CemsGameApp/Pong/pong_game.kv')
         Builder.load_file('../CemsGameApp/BlackJack/blackjack_game.kv')
+        Builder.load_file('splashScreen.kv')
+        Builder.load_file('login.kv')
         Builder.load_file('menu.kv')
         Builder.load_file('settings.kv')
 
         sm = MDScreenManager()
+        sm.add_widget(SplashScreen(name='splash screen'))
+        sm.add_widget(LoginScreen(name='login'))
         sm.add_widget(MenuScreen(name='menu'))
         sm.add_widget(SettingsScreen(name='settings'))
         sm.add_widget(PongGameScreen(name='pong game'))
         return sm
 
     def on_start(self):
-        # Window.fullscreen = 'auto'
-        Window.size = (1800, 900)
+        Window.size = (2556, 1179)
+
+        screen_width = Window.width
+        screen_height = Window.height
+        window_width = self.root.width
+        window_height = self.root.height
+
+        x = screen_width - window_width / 2
+        y = screen_height - window_height / 2
+
+        Window.position = 'custom'
+        Window.top = y
+        Window.left = x
+
+        Clock.schedule_once(self.change_screen, 3)
+
+    def change_screen(self, *args):
+        self.root.transition = FadeTransition(duration=0.25)
+        self.root.current = 'login'
 
 
 if __name__ == '__main__':
